021dcb8890bc21047dd5bcab5d28aaf8e090ebc4
Piotr Nowek <piotrek21125@wp.pl>
2024-07-10 15:18:05 +0200

    Pytest update pet final version

--- tests/api/pet/test_pet.py
+++ tests/api/pet/test_pet.py
@@ -28,18 +28,77 @@ def test_create_new_pet():
         "status": "available"
     }
     create_pet_response = create_pet(payload)
+    assert create_pet_response.status_code == 200
     data = create_pet_response.json()
     
     pet_id = data["id"]
     get_pet_by_id_response = function_get_pet_by_id(pet_id)
+    assert get_pet_by_id_response.status_code == 200
     
     get_pet_by_id = get_pet_by_id_response.json()
     assert get_pet_by_id["id"] == pet_id
     assert get_pet_by_id["name"] == payload["name"]
     assert get_pet_by_id["status"] == payload["status"]
 
+def test_update_pet():
+    payload = {
+        "id": 0,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "dog",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    }
+    create_pet_response = create_pet(payload)
+    assert create_pet_response.status_code == 200
+
+    new_payload = {
+        "id": 0,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "cat",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "sold"
+    }
+    udate_pet_response = update_pet(new_payload)
+    assert udate_pet_response.status_code == 200
+
+    data = udate_pet_response.json()
+    pet_id = data["id"]
+    get_pet_by_id_response = function_get_pet_by_id(pet_id)
+    assert get_pet_by_id_response.status_code == 200
+    get_pet_by_id = get_pet_by_id_response.json()
+    assert get_pet_by_id["id"] == pet_id
+    assert get_pet_by_id["name"] == new_payload["name"]
+    assert get_pet_by_id["status"] == new_payload["status"]
+
+
+
 def create_pet(payload):
     return requests.post(ENDPOINT + "/v2/pet", json=payload)
 
 def function_get_pet_by_id(pet_id):
     return requests.get(ENDPOINT + f"/v2/pet/{pet_id}")
+
+def update_pet(payload):
+    return requests.put(ENDPOINT + "/v2/pet", json=payload)
