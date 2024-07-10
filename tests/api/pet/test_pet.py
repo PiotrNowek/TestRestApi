bbcd27137867338a8ad10241e4c6479e14fddcd0
Piotr Nowek <piotrek21125@wp.pl>
2024-07-10 12:06:21 +0200

    Working test_create_new_pet

--- tests/api/pet/test_pet.py
+++ tests/api/pet/test_pet.py
@@ -1,12 +1,33 @@
 import requests
 
 
-ENDPOINT = "https://petstore.swagger.io/v2/pet/findByStatus?status=pending"
+ENDPOINT = "https://petstore.swagger.io"
 
 
 def test_check_endpoint():
     response = requests.get(ENDPOINT)
     assert response.status_code == 200
-    pass
-
-
+    
+def test_create_new_pet():
+    payload = {
+        "id": 0,
+        "category": {
+            "id": 0,
+            "name": "string"
+        },
+        "name": "dog",
+        "photoUrls": [
+            "string"
+        ],
+        "tags": [
+            {
+                "id": 0,
+                "name": "string"
+            }
+        ],
+        "status": "available"
+    }
+    response = requests.post(ENDPOINT + "/v2/pet", json=payload)
+    data = response.json()
+    print(data)
+    
