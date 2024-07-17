b226c06f821f19bb8166cab4d5f1759c11630a72
Piotr Nowek <piotrek21125@wp.pl>
2024-07-17 12:09:31 +0200

    test_pet_negative updated version

--- tests/api/pet/test_pet_negative.py
+++ tests/api/pet/test_pet_negative.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 
@@ -81,15 +82,50 @@ def test_add_pet_with_empty_id():
     
 
 def test_get_pet_by_invalid_id():
-    invalid_pet_id = 999999999
+    invalid_pet_id = 9999999991 # We assume this ID does not exist
     response = requests.get(ENDPOINT + f"/v2/pet/{invalid_pet_id}")
     assert response.status_code == 404, f"Expected status code 404, but got {response.status_code}"
+    response_data = response.json()
+    assert "message" in response_data, "Expected 'message' to be in the response data"
+    assert response_data["message"] == "Pet not found", f"Expected error message 'Pet not found', but got {response_data['message']}"
+    assert response_data["type"] == "error", f"Expected type message 'error', but got {response_data['type']}"
 
 
+@pytest.fixture
+def non_exist_pet():
+    return {
+        "id" : 999999999, # We assume this ID does not exist
+        "category" : {
+            "id" : 0,
+            "name" : "string"
+        },
+        "name" : "Ghost",
+        "photoUrls" : [
+            "string"
+        ],
+        "tags" : [
+            {
+                "id" : 0,
+                "name" : "string"
+            }
+        ],
+        "status" : "available"
+    }
+
+
+def test_update_nonexist_pet(non_exist_pet):
+    response = requests.put(ENDPOINT + "/v2/pet", json=non_exist_pet)
+    assert response.status_code == 404, f"Expected status code 404, but got {response.status_code}"
+    response_data = response.json()
+    assert "message" in response_data, "Expected 'message' to be in response data"
+    assert response_data["message"] == "Pet not found", f"Expected error message 'Pet not found', but got {response_data['message']}"
   
 
-
-
+def test_delete_nonexist_pet():
+    non_exist_pet_id = 8888888 # We assume this ID does not exist
+    response = requests.delete(ENDPOINT + f"/v2/pet/{non_exist_pet_id}")
+    assert response.status_code == 404, f"Expected status code 404, but got {response.status_code}"
+ 
 
 
 
